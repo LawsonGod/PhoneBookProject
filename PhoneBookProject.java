9947d0d87835d1172da9657b963429a67dfc9d2f
LawsonGod <godwill.lawson@gmail.com>
2023-11-19 12:19:14 +0100

    amélioration du code

--- PhoneBookProject.java
+++ PhoneBookProject.java
@@ -1,28 +1,27 @@
-package PhoneBookProject;
+package LAWSON_PhoneBookProject;
 
 import java.util.Scanner;
 
 public class PhoneBookProject {
+    public static Scanner sc = null;
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Entrez un nom de famille: ");
-        String userLastName = sc.nextLine();
+       sc = new Scanner(System.in);
 
-        System.out.println("Entrez un Prénom: ");
-        String userFirstName = sc.nextLine();
-
-        System.out.println("Entrez un numéro de téléphone: ");
-        String userPhoneNumber = sc.nextLine();
-
-
+        String userLastName = getUserInput("Entrez un nom de famille: ");
+        String userFirstName = getUserInput("Entrez un Prénom");
+        String userPhoneNumber = getUserInput("Entrez un numéro de téléphone");
 
         System.out.println(userLastName);
         System.out.println(userFirstName);
         System.out.println(userPhoneNumber);
 
         sc.close();
+    }
+    public  static String getUserInput(String userRequest){
 
+        System.out.println(userRequest);
+        return sc.nextLine();
     }
 
 }
