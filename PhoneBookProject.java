5a36e23252e02a29bbccf6ecc4dde06e49de6767
LawsonGod <godwill.lawson@gmail.com>
2023-11-19 17:15:41 +0100

    Ajout d'un fichier pour lister les contacts + test pour vérifier l'existence du fichier .txt

--- PhoneBookProject.java
+++ PhoneBookProject.java
@@ -1,5 +1,6 @@
 package LAWSON_PhoneBookProject;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class PhoneBookProject {
@@ -8,15 +9,21 @@ public class PhoneBookProject {
 
        sc = new Scanner(System.in);
 
-        String userLastName = getUserInput("Entrez un nom de famille: ");
+      /*  String userLastName = getUserInput("Entrez un nom de famille: ");
         String userFirstName = getUserInput("Entrez un Prénom");
         String userPhoneNumber = getUserInput("Entrez un numéro de téléphone");
 
         Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
 
+        System.out.println(newContact.toString());*/   /* Sans le toString le systeme print out renvoi un sort de code ASCII qui fait reference à sa place dans la mémoire*/
 
-        System.out.println(newContact.toString());   /* Sans le toString le systeme print out renvoi un sort de code ASCII qui fait reference à sa place dans la mémoire*/
-
+        File phoneBookFile = new File("/Users/godwilllawson/Desktop/PhoneBookContact/LAWSON_PhoneBookProject/phoneBook_contact.txt");
+        if(phoneBookFile.exists()){
+            System.out.println("Le fichier existe à l'emplacement donné");
+        }
+        else {
+        System.out.println("Le fichier n'existe pas");
+            }
         sc.close();
     }
     public  static String getUserInput(String userRequest){
