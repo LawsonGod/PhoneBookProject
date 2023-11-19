3a30745cd9eddf959234a14d3bd67fe1570507ca
LawsonGod <godwill.lawson@gmail.com>
2023-11-19 18:33:32 +0100

    Finalisation de l'annuaire phoneBook

--- PhoneBookProject.java
+++ PhoneBookProject.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class PhoneBookProject {
     public static Scanner sc = null;
+    public static final String PHONE_BOOK_FILE_PATH = "/Users/godwilllawson/Desktop/PhoneBookContact/LAWSON_PhoneBookProject/phoneBook_contact.txt";
     public static void main(String[] args) {
 
        sc = new Scanner(System.in);
@@ -19,38 +20,43 @@ public class PhoneBookProject {
         Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
 
         System.out.println(newContact.toString());   /* Sans le toString le systeme print out renvoi un sort de code ASCII qui fait reference à sa place dans la mémoire*/
+        File phoneBookFile = getOrCreatePhoneBookFile(PHONE_BOOK_FILE_PATH);
+        appendContactToPhoneBook(phoneBookFile, newContact);
 
-        File phoneBookFile = new File("/Users/godwilllawson/Desktop/PhoneBookContact/LAWSON_PhoneBookProject/phoneBook_contact.txt");
-
-
-        if(phoneBookFile.exists()){
-            System.out.println("Le fichier existe à l'emplacement donné");
-        }
-        else {
-            System.out.println("Le fichier n'existe pas");
-
-            try {
-                phoneBookFile.createNewFile();
-                System.out.println("Le fichier à été créé");
-            }  catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        try {
-            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true));
-            fileWriter.append(newContact.toString() + "\n");
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         sc.close();
     }
     public  static String getUserInput(String userRequest){
-
         System.out.println(userRequest);
         return sc.nextLine();
     }
+    public static File getOrCreatePhoneBookFile(String phoneBookFilePath){
+
+        File phoneBookFile = new File(phoneBookFilePath);
+
+        if(phoneBookFile.exists()){
+            return phoneBookFile;
+        }
+            try {
+                phoneBookFile.createNewFile();
+                System.out.println("Le fichier à été créé dans (" + phoneBookFilePath + ")");
+                return phoneBookFile;
+            }  catch (IOException e) {
+                e.printStackTrace();
+            }
+        return null ;
+    }
+    public static void appendContactToPhoneBook(File phoneBookFile, Contact newContact){
+
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true));){
+
+            fileWriter.append(newContact.toString() + "\n");
+            System.out.println("Contact ajouté");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
